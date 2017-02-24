3335e9e7e30144ef96202c73f155bf1f9e520c24
shiga <shigachen@outlook.com>
2017-02-24 15:58:18 +0800

    来挑战看看~

---
+++ js/roles/timer.js
@@ -0,0 +1,16 @@
+define([],function () {
+	function Timer() {
+
+	}
+
+	Timer.prototype = {
+		draw: function() {
+			console.log('绘制计时器');
+		},
+
+		update: function() {
+			console.log('更新计时器');
+		}
+	}
+	return Timer;
+});
